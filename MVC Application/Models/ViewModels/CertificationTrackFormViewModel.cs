b4eacd331dce9f8b4d188df6f50f92c010bacb27
ffawants <fatimaalmajid05@gmail.com>
2026-04-24 12:53:21 +0300

    Added Certification Track management pages (CRUD)

---
+++ MVC Application/Models/ViewModels/CertificationTrackFormViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Application.Models.ViewModels
+{
+    public class CertificationTrackFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Track Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Display(Name = "Required Courses")]
+        public List<int> SelectedCourseIds { get; set; } = new();
+
+        public List<SelectListItem> CourseOptions { get; set; } = new();
+    }
+}
